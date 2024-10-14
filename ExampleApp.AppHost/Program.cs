d3c3b59c78b29fba3c63e6406843ba8e28ea13e4
pavel <lutskovp@gmail.com>
2024-10-13 23:55:31 -0700

    feat: fixes /products service port

--- ExampleApp.AppHost/Program.cs
+++ ExampleApp.AppHost/Program.cs
@@ -1,6 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Store>("store");
-builder.AddProject<Projects.Products>("products");
+builder.AddProject<Projects.Products>("products")
+    .WithHttpEndpoint(port: 5200, name: "products");
 
 builder.Build().Run();
