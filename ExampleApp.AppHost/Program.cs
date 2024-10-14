379f3109e6036652701901eebefe24fc0414e2cf
pavel <lutskovp@gmail.com>
2024-10-13 23:44:19 -0700

    feat: updates app host to monitor major components

--- ExampleApp.AppHost/Program.cs
+++ ExampleApp.AppHost/Program.cs
@@ -1,3 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.Store>("store");
+builder.AddProject<Projects.Products>("products");
+
 builder.Build().Run();
