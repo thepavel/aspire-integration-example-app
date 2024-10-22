5501337d9512778847767ea80a40c9e73c1aa195
pavel <lutskovp@gmail.com>
2024-10-21 23:06:03 -0700

    feat: adds postgres integration

--- ExampleApp.AppHost/Program.cs
+++ ExampleApp.AppHost/Program.cs
@@ -1,4 +1,8 @@
+
+
 var builder = DistributedApplication.CreateBuilder(args);
+var postgres = builder.AddPostgres("postgres");
+var postgresdb = postgres.AddDatabase("postgresdb");
 
 var products = builder.AddProject<Projects.Products>("products");
 
