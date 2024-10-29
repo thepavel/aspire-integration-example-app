4bc15f533ce2a7f670538296b6e7354c7b85bc87
pavel <lutskovp@gmail.com>
2024-10-28 22:49:02 -0700

    feat: fix build errors

--- start/Basket.API/Models/CustomerBasket.cs
+++ start/Basket.API/Models/CustomerBasket.cs
@@ -2,6 +2,11 @@
 
 public class CustomerBasket
 {
+    /// <summary>
+    /// MongoDB document identifier
+    /// </summary>
+    public string _id { get; set; } = "";
+    
     public required string BuyerId { get; set; }
 
     public List<BasketItem> Items { get; set; } = [];
